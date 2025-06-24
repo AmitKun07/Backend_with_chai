5f98386de69291c9978533dd775fc055c7bedf3b
Amitkun07 <amitkun07@gmail.com>
2025-06-24 22:56:55 +0530

    project setup04

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import server from "./server.js";
+import io from "./socket.js";
+import connectDB from "./db/index.js";
+
+// Load environment variables from .env file
+dotenv.config({ path: "../.env" });
+
+process.setMaxListeners(15);
+
+console.log(process.env.MONGO_URI)
+
+const serverPort = process.env.PORT || 7000;
+
+connectDB()
+  .then(() => {
+    server.listen(serverPort, () => {
+      console.log({
+        serverStatus: "🌐  Application is Running",
+        URL: `🔗 http://localhost:${serverPort}`,
+      });
+    });
+
+    server.on("error", (err) => {
+      console.log("Error occurred at index.js", err);
+    });
+  })
+  .catch((error) => {
+    console.log("DB connection failed from index.js", error);
+  });
+  console.log('MongoDB URI:', process.env.MONGO_URI);
